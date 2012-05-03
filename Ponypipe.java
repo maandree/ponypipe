62a918773e114e8a205ad88dd9c75003ed14d118
Mattias Andrée <maandree@operamail.com>
2012-05-03 22:35:35 +0200

    decode and encode utf8

--- Ponypipe.java
+++ Ponypipe.java
@@ -43,7 +43,7 @@ public class Ponypipe
 	final OutputStream decodePipe   = new DecodeStream(ponify ? ponifyPipe : deponify ? deponifyPipe : encodePipe);
 	
 	for (int b; (b = System.in.read()) != -1;)
-	    encodePipe.write(b);
+	    decodePipe.write(b);
     }
     
     
@@ -55,10 +55,31 @@ public class Ponypipe
 	}
 	
 	private final OutputStream next;
+	private int n = 0;
+	private int buf = 0;
 	
 	public void write(final int b) throws IOException
 	{
-	    next.write(b);
+	    if ((b & 0x80) == 0)
+		this.next.write(b);
+	    else if ((b & 0xC0) == 0xC0)
+	    {
+		n = 0;
+		buf = b;
+		while ((buf & 0x80) == 0x80)
+		{
+		    n++;
+		    buf <<= 1;
+		}
+		buf = (buf & 0xFF) >> n--;
+	    }
+	    else
+		if (n > 0)
+		{
+		    buf = (buf << 6) | (b & 1023);
+		    if (--n == 0)
+			this.next.write(buf);
+		}
 	}
     }
     
@@ -71,10 +92,38 @@ public class Ponypipe
 	}
 	
 	private final OutputStream next;
+	private final int[] buf = new int[6];
 	
 	public void write(final int b) throws IOException
 	{
-	    next.write(b);
+	    if (b < 0x80)
+		this.next.write(b);
+	    else
+	    {
+		int m = 0x100;
+		int d = b;
+		int ptr = 0;
+		for (;;)
+	        {
+		    m |= m >> 1;
+		    this.buf[ptr++] = d & 1023;
+		    d >>>= 6;
+		    if (d == 0)
+		    {
+			m >>= 1;
+			if ((m & this.buf[ptr - 1]) == 0)
+			    this.buf[ptr - 1] |= (m << 1) & 0xFF;
+			else
+			    this.buf[ptr++] = m;
+			break;
+		    }
+		}
+		
+		while (ptr > 0)
+		    this.next.write(this.buf[--ptr]);
+		
+		this.buf[0] = this.buf[1] = this.buf[2] = this.buf[3] = this.buf[4] = this.buf[5] = 0;
+	    }
 	}
     }
     
